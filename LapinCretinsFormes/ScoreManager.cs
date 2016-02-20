e00b895d032db3184f7de73958b6e5e4f2081064
Nawhal <nawhal.sayarh@etu.udamail.fr>
2016-02-20 22:56:29 +0100

    Commented and functional ScoreManager
    Rajouté les commentaires et un x100 oublié.

--- LapinCretinsFormes/ScoreManager.cs
+++ LapinCretinsFormes/ScoreManager.cs
@@ -7,8 +7,17 @@ using System.Windows.Media;
 
 namespace LapinCretinsFormes
 {
+    /// <summary>
+    /// Manages how to calculate the score.
+    /// </summary>
     public class ScoreManager
     {
+        /// <summary>
+        /// Calculates the score from the shape the players have to fill and their overall shadow.
+        /// </summary>
+        /// <param name="shapeToFill">The shape players have to fill.</param>
+        /// <param name="peoplesShadows">The players' overall shadow.</param>
+        /// <returns>The players' score.</returns>
         public int ScoreFromGeometries(Geometry shapeToFill, Geometry peoplesShadows)
         {
             double maximumScore = shapeToFill.GetArea();
@@ -17,11 +26,17 @@ namespace LapinCretinsFormes
             return score > 0 ? (int)score : 0;
         }
 
+        /// <summary>
+        /// Calculates the percentage of the shape the players have to fill that is filled by their shadow.
+        /// </summary>
+        /// <param name="shapeToFill">The shape players have to fill.</param>
+        /// <param name="peoplesShadows">The players' overall shadow.</param>
+        /// <returns>The percentage of the shape that is filled by the players' overall shadow.</returns>
         public float FilledPercentageFromGeometries(Geometry shapeToFill, Geometry peoplesShadows)
         {
             double maximumArea = shapeToFill.GetArea();
             double filledArea = Geometry.Combine(shapeToFill, peoplesShadows, GeometryCombineMode.Intersect, null).GetArea();
-            double percentage = filledArea / maximumArea;
+            double percentage = 100 * filledArea / maximumArea;
             return (float) Math.Round(percentage, 2);
         }
     }
