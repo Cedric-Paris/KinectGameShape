6492f2d919d583894dabb3e067539c809b3f6adf
Nawhal <nawhal.sayarh@etu.udamail.fr>
2016-02-20 22:39:08 +0100

    Add ScoreManager
    Calcule le score et le pourcentage de remplissage.

---
+++ LapinCretinsFormes/ScoreManager.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Media;
+
+namespace LapinCretinsFormes
+{
+    public class ScoreManager
+    {
+        public int ScoreFromGeometries(Geometry shapeToFill, Geometry peoplesShadows)
+        {
+            double maximumScore = shapeToFill.GetArea();
+            double penalties = Geometry.Combine(shapeToFill, peoplesShadows, GeometryCombineMode.Xor, null).GetArea();
+            double score = maximumScore - penalties;
+            return score > 0 ? (int)score : 0;
+        }
+
+        public float FilledPercentageFromGeometries(Geometry shapeToFill, Geometry peoplesShadows)
+        {
+            double maximumArea = shapeToFill.GetArea();
+            double filledArea = Geometry.Combine(shapeToFill, peoplesShadows, GeometryCombineMode.Intersect, null).GetArea();
+            double percentage = filledArea / maximumArea;
+            return (float) Math.Round(percentage, 2);
+        }
+    }
+}
