faba52266e2bb13e254671e618cc5465b9e99c85
Nawhal <nawhal.sayarh@etu.udamail.fr>
2016-03-03 12:46:51 +0100

    Début d'interface graphique
    - Menu Principal fonctionnel
    - Fenetre d'instructions à finaliser
    - Fenetre de score (après jeu) à finaliser
    - Classe Shape afin d'avoir une image associée à notre Geometry pour
    avoir un background qui correspond avec la forme

---
+++ LapinCretinsFormes/MenuPrincipal.xaml.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace LapinCretinsFormes
+{
+    /// <summary>
+    /// Interaction logic for MenuPrincipal.xaml
+    /// </summary>
+    public partial class MenuPrincipal : Window
+    {
+        public MenuPrincipal()
+        {
+            InitializeComponent();
+        }
+
+        private void CloseButtonClick(object sender, RoutedEventArgs e)
+        {
+            this.Close();
+        }
+
+        private void NewGameButtonClick(object sender, RoutedEventArgs e)
+        {
+            Instructions i = new Instructions();
+            i.Show();
+            Close();
+        }
+    }
+}
---
+++ LapinCretinsFormes/Shape.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Media;
+
+namespace LapinCretinsFormes
+{
+    public class Shape
+    {
+        private Geometry _shapeGeometry;
+            public Geometry getShapeGeometry() { return _shapeGeometry; }
+
+        private Uri _imageUri;
+            public Uri getImageUri() { return _imageUri; }
+
+        public Shape(Geometry shapeGeometry, Uri image)
+        {
+            _shapeGeometry = shapeGeometry;
+            _imageUri = image;
+        }
+    }
+}
